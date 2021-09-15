8119728756964e36fbde307d5010a5e5111a44e6
yrojx <yossansr06@gmail.com>
2021-09-15 13:27:53 +0700

    first commit

---
+++ TryTravis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryTravis
+//
+//  Created by Yossan Rahmadi on 15/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TryTravisTests/TryTravisTests.swift
@@ -0,0 +1,26 @@
+//
+//  TryTravisTests.swift
+//  TryTravisTests
+//
+//  Created by Yossan Rahmadi on 15/09/21.
+//
+
+import XCTest
+@testable import TryTravis
+
+class TryTravisTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
